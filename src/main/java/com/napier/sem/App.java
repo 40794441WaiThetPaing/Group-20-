a817f238fc3e0f14fd2bf64b1e501aa7046a33ad
40794434 <40794434@live.napier.ac.uk>
2025-10-13 11:15:31 +0630

    Create a new class "City"

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -59,6 +59,7 @@ public class App {
         }
     }
 
+
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.class.path"));
         // Create new Application
---
+++ src/main/java/com/napier/sem/City.java
@@ -0,0 +1,13 @@
+package com.napier.sem;
+
+public class City {
+    public int ID;
+
+    public String Name;
+
+    public String CountryCode;
+
+    public String District;
+
+    public int population;
+}
