0b5ff61412682beb6ce9fc972ef5a8c27addb766
ElJefeNathan <kamennathangamer@gmail.com>
2025-10-13 21:28:38 +0630

    Created a separate class for total population reports

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -1,7 +1,6 @@
 package com.napier.sem;
 
 import java.sql.*;
-import java.util.ArrayList;
 
 public class App {
 
@@ -38,7 +37,7 @@ public class App {
                 System.out.println("Successfully connected");
                 break;
             } catch (SQLException sqle) {
-                System.out.println("Failed to connect to database attempt " + Integer.toString(i));
+                System.out.println("Failed to connect to database attempt " + i);
                 System.out.println(sqle.getMessage());
             } catch (InterruptedException ie) {
                 System.out.println("Thread interrupted? Should not happen.");
@@ -52,107 +51,11 @@ public class App {
     public void disconnect() {
         if (con != null) {
             try {
-                // Close connection
                 con.close();
             } catch (Exception e) {
                 System.out.println("Error closing connection to database");
             }
         }
-    }
-
-    /**
-     * Retrieves the total population of the world from the 'country' table.
-     */
-    public long getTotalWorldPopulation() {
-        long totalPopulation = 0;
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect = "SELECT SUM(Population) AS TotalWorldPopulation\n" +
-                    "FROM country;";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Retrieve result
-            if (rset.next()) {
-                totalPopulation = rset.getLong("TotalWorldPopulation");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get total world population");
-        }
-        return totalPopulation;
-    }
-
-    /**
-     * Prints the total world population.
-     */
-    public void printTotalWorldPopulation(long population) {
-        System.out.printf("%-30s %-12d\n", "Total World Population:", population);
-    }
-
-    /**
-     * Retrieves the total population of a given continent.
-     * Example: "North America"
-     */
-    public long getContinentPopulation(String continentName) {
-        long continentPopulation = 0;
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // SQL query to sum population for the given continent
-            String strSelect = "SELECT SUM(Population) AS ContinentPopulation " +
-                    "FROM country WHERE Continent = '" + continentName + "';";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Retrieve result
-            if (rset.next()) {
-                continentPopulation = rset.getLong("ContinentPopulation");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get continent population");
-        }
-        return continentPopulation;
-    }
-
-    /**
-     * Prints the total population of a given continent.
-     */
-    public void printContinentPopulation(String continentName, long population) {
-        System.out.printf("%-30s %-12d\n", "Population of " + continentName + ":", population);
-    }
-
-    /**
-     * Retrieves the total population of a given region.
-     * Example: "Caribbean"
-     */
-    public long getRegionPopulation(String regionName) {
-        long regionPopulation = 0;
-        try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // SQL query to sum population for the given region
-            String strSelect = "SELECT SUM(Population) AS RegionPopulation " +
-                    "FROM country WHERE Region = '" + regionName + "';";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Retrieve result
-            if (rset.next()) {
-                regionPopulation = rset.getLong("RegionPopulation");
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get region population");
-        }
-        return regionPopulation;
-    }
-
-    /**
-     * Prints the total population of a given region.
-     */
-    public void printRegionPopulation(String regionName, long population) {
-        System.out.printf("%-30s %-12d\n", "Population of " + regionName + ":", population);
     }
 
     public static void main(String[] args) {
@@ -162,17 +65,20 @@ public class App {
         // Connect to database
         a.connect();
 
+        // Create instance of report generator
+        GeneralPopulationReports reports = new GeneralPopulationReports(a.con);
+
         // Get and print total world population
-        long totalPopulation = a.getTotalWorldPopulation();
-        a.printTotalWorldPopulation(totalPopulation);
+        long totalPopulation = reports.getTotalWorldPopulation();
+        reports.printTotalWorldPopulation(totalPopulation);
 
         // Get and print total population for North America
-        long northAmericaPop = a.getContinentPopulation("North America");
-        a.printContinentPopulation("North America", northAmericaPop);
+        long northAmericaPop = reports.getContinentPopulation("North America");
+        reports.printContinentPopulation("North America", northAmericaPop);
 
         // Get and print total population for Caribbean
-        long caribbeanPop = a.getRegionPopulation("Caribbean");
-        a.printRegionPopulation("Caribbean", caribbeanPop);
+        long caribbeanPop = reports.getRegionPopulation("Caribbean");
+        reports.printRegionPopulation("Caribbean", caribbeanPop);
 
         // Disconnect from database
         a.disconnect();
